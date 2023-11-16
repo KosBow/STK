eb4211720e06d0266048ac573b1316a275cb4397
Khosrat A <kosrat_95@hotmail.com>
2023-11-16 03:13:06 +0100

    almost finished

--- scripts/localStorage.js
+++ scripts/localStorage.js
@@ -1,51 +1,72 @@
-function saveToLocalStorage(key, value) {
-    localStorage.setItem(key, value)
-  }
-
-  function getFromLocalStorage(key) {
-    return localStorage.getItem(key)
-  }
-
-  function setLocalStorage() {
-    document.addEventListener("DOMContentLoaded", function () {
-      const form = document.querySelector(".login-box form")
-
-      form.addEventListener("submit", function (event) {
-        event.preventDefault()
-
-        const inputs = form.querySelectorAll("input")
-
-        inputs.forEach((input) => {
-          const label = input.getAttribute("label")
-          const value = input.value
-
-          if (label === "Tv-Titel") {
-            saveToLocalStorage("Tv-Titel", value)
-          } else if (label === "Åldersrekommendation") {
-            saveToLocalStorage("Åldersrekommendation", value)
-          } else if (label === "Beskrivning") {
-            saveToLocalStorage("Beskrivning", value)
-          }
-        })
-        showStorage()
-      })
-      showStorage()
-    })
-  }
-
-function showStorage(){
-    const localTracker = document.querySelector(".localShower")
-    const localButton = document.querySelector(".showLocal")
-    localButton.addEventListener("click", ()=>{
-        const TvTitel = getFromLocalStorage("Tv-Titel")
-        const Åldersrekommendation = getFromLocalStorage("Åldersrekommendation")
-        const Beskrivning = getFromLocalStorage("Beskrivning")
-        if(localTracker){
-            localTracker.innerHTML = `
-            <p>Tv Titel:${TvTitel}</p>
-            `
-        }
-    })
+function setItemToLocalStorage(key, value) {
+  localStorage.setItem(key, value) //funktion som vi använder senare för att lägga till item i  localStorage
 }
 
-setLocalStorage()
+function getItemFromLocalStorage(key) {
+  localStorage.getItem(key) //funktion som vi använder senare för att hämta från localStorage
+}
+
+function localStorageSave(objectKey, data) {
+  //detta funktionen ser till att vi sparar all ny data i en array som vi lägger i en objectKey som vi senare kan söka genom
+  const SparadData = JSON.parse(localStorage.getItem(objectKey)) || []
+  SparadData.push(data)
+  localStorage.setItem(objectKey, JSON.stringify(SparadData))
+}
+
+function localStorageSet() {
+  //vi anväder detta för att se vad användaren har mattat in som vi sparar i en localstorage genom att använda oss av functionen localStorageShow()
+  document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector(".info-box form")
+
+    form.addEventListener("submit", (event) => {
+      event.preventDefault()
+
+      const inmatning = form.querySelectorAll("input")
+      const nyData = {}
+
+      inmatning.forEach((input) => {
+        const label = input.getAttribute("label")
+        const value = input.value
+
+        nyData[label] = value
+      })
+      localStorageSave("programData", nyData)
+      form.reset()
+      localStorageShow()
+    })
+    localStorageShow()
+  })
+}
+function localStorageShow() {
+  //hämtar ut datan från localStorage
+  const sparadData = JSON.parse(localStorage.getItem("programData"))
+  searchDisplayer(sparadData)
+}
+
+function searchDisplayer(data) {
+  //detta visar resultatet i html koden genom sökning till ordet exempel = breaking bad så visar den resultatet som har breaking i sig
+  searchInmatning = document.querySelector(".box input")
+  const searchResultat = document.querySelector(".search-results")
+
+  searchInmatning.addEventListener("input", () => {
+    const searchT = searchInmatning.value.toLowerCase()
+    searchResultat.innerHTML = ""
+    if (searchT.trim() === "") {
+      return
+    }
+    data.forEach((item) => {
+      const title = item["Tv-Titel"].toLowerCase()
+
+      if (title.startsWith(searchT)) {
+        const resultObject = document.createElement("div")
+        resultObject.innerHTML = `
+          <h3 class="resultat-titel">${item["Tv-Titel"]}</h3>
+          <p class="resultat-text">Åldersrekommendation: ${item["Åldersrekommendation"]}</p>
+          <p class="resultat-text">Beskrivning: ${item["Beskrivning"]}</p>
+           `
+        searchResultat.appendChild(resultObject)
+      }
+    })
+  })
+}
+localStorageSet()
